dfe5f168012312bbef2770db57f86c27c4561ac8
SadraKian <sadrakiandeveloper@gmail.com>
2023-08-08 12:05:36 +0330

    Adding some elements to prompt card component

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,15 +2,13 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const GET = async (request) => {
-    try {
-        await connectToDB();
+  try {
+    await connectToDB();
 
-        const prompts = Prompt.find({}).populate('creator');
-        
-        return new Response(JSON.stringify(prompts), {status : 200});
-    } catch (error) {
-        return new Response("Failed to fetch all prompts!", { status: 500 })
-    }
-    
+    const prompts = await Prompt.find({}).populate("creator");
 
-}
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts!", { status: 500 });
+  }
+};
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
-function PromptCard() {
+function PromptCard({ post, handleTagClick, handleDelete, handleEdit }) {
+  const [copied, setCopied] = useState(false);
   return (
-    <div>PromptCard</div>
-  )
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center cursor-pointer gap-3">
+          <Image
+            src={post.creator.image}
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500 ">
+              {post.creator.emaail}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn">
+          <Image src="/assets/images/copy.png" width={17} height={17} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default PromptCard
+export default PromptCard;
