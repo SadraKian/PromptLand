34284a9a301aa6f617d9140391223100d27fc350
SadraKian <sadrakiandeveloper@gmail.com>
2023-08-11 17:07:50 +0330

    Adding faveIcon

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,18 +9,19 @@ export const metadata = {
 function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/assets/images/logo.svg" type="image/x-icon" />
+      </head>
       <body>
         <Provider>
-          
-        <div className="main">
-          <div className="gradient" />
-        </div>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">
-          <Nav/>
-          {children}
-        </main>
-
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
         </Provider>
       </body>
     </html>
